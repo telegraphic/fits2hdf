40a0056f2cc13cb2ab2b2f224bad1eafc0b0d1c2
Danny Price <dan@thetelegraphic.com>
2015-02-25 21:32:54 -0500

    Update documentation and docstrings

--- fits2fits.py
+++ fits2fits.py
@@ -4,7 +4,19 @@
 fits2hdf.py
 ===========
 
-FITS to HDF5 conversion utility. This script takes a
+FITS to FITS conversion utility. This script takes a FITS file, loads it into
+an in-memory data structure, then outputs a FITS file. This utility is provided
+for testing, and so that you can see how your data may be affected by hdf2fits.
+
+Notes
+------
+
+The FITS writing routines in fits2hdf only output binary tables and images. So,
+if your original data was an ASCII table, or a random group, then it won't come
+out quite the same. However, the contents of data should be the same.
+Similarly, you can expect to lose a few header keywords on mandatory things like
+BITPIX and SIMPLE. Units are parsed and converted into units that astropy 
+agrees with.
 """
 
 import os
