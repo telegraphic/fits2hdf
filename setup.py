ba15bc93e3518d160ee66d15808988b09b0c3eca
Danny Price <dan@thetelegraphic.com>
2015-05-28 12:04:41 +0100

    Added entry points so setup installs command line scripts
    See https://github.com/telegraphic/fits2hdf/issues/5

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
+"""
+setup.py -- setup script for fits2hdf package
+"""
+
 from setuptools import setup, Extension, Distribution, find_packages
 
+# create entry points
+# see http://astropy.readthedocs.org/en/latest/development/scripts.html
+entry_points = {
+    'console_scripts' :
+        ['fits2hdf = fits2hdf.file_conversion:convert_fits_to_hdf',
+         'hdf2fits = fits2hdf.file_conversion:convert_hdf_to_fits',
+         'fits2fits = fits2hdf.file_conversion:convert_fits_to_fits']
+    }
 
 setup(name='fits2hdf',
-      version='0.2',
+      version='1.0',
       description='FITS to HDF5 conversion utility',
       url='http://github.com/telegraphic/fits2hdf',
       author='Danny Price',
       author_email='dprice@cfa.harvard.edu',
       license='MIT',
       packages=find_packages(),
-      zip_safe=False)
+      zip_safe=False,
+      entry_points = entry_points
+      )
