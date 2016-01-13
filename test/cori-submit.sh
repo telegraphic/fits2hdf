4acd1ac3f60d602ec61e203c86af83680558e118
valiantljk <jaln.liu@ttu.edu>
2016-01-13 14:56:49 -0800

    serial/parallel h5fits

--- test/cori-submit.sh
+++ test/cori-submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p debug 
-#SBATCH -N 1
-#SBATCH -t 00:10:00
+#SBATCH -N 2
+#SBATCH -t 00:20:00
 #SBATCH -J fits-hdf
 #SBATCH -e %j.err
 #SBATCH -o %j.out
 #SBATCH -V
 
 cd $SLURM_SUBMIT_DIR
-python -W ignore test_multihdf-sample.py  32
+python  h5fits-parallel.py  64 
